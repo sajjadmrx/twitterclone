d4d9c9751f62280a04987634f5fb9fed66ef2c3d
Reece Kenney <reecekenney50@gmail.com>
2020-10-14 18:14:51 +0100

    registerRoute

--- app.js
+++ app.js
@@ -13,9 +13,10 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // Routes
 const loginRoute = require('./routes/loginRoutes');
+const registerRoute = require('./routes/registerRoutes');
 
 app.use("/login", loginRoute);
-
+app.use("/register", registerRoute);
 
 app.get("/", middleware.requireLogin, (req, res, next) => {
 
