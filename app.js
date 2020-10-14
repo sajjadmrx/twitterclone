46e839c30779cf4644c780deac7a50a4ed78aba3
Reece Kenney <reecekenney50@gmail.com>
2020-10-14 16:41:57 +0100

    staticFiles

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ const express = require('express');
 const app = express();
 const port = 3003;
 const middleware = require('./middleware')
+const path = require('path')
 
 const server = app.listen(port, () => console.log("Server listening on port " + port));
 
 app.set("view engine", "pug");
 app.set("views", "views");
+
+app.use(express.static(path.join(__dirname, "public")));
 
 // Routes
 const loginRoute = require('./routes/loginRoutes');
