957db366c8bef378bfd7a9971e8f1cee47104dcd
Reece Kenney <reecekenney50@gmail.com>
2020-10-14 20:19:29 +0100

    bodyParser

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const app = express();
 const port = 3003;
 const middleware = require('./middleware')
 const path = require('path')
+const bodyParser = require("body-parser")
 
 const server = app.listen(port, () => console.log("Server listening on port " + port));
 
 app.set("view engine", "pug");
 app.set("views", "views");
 
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 // Routes
