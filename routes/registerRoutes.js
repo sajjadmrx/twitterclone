2fc067471964b9b23236f6ffb1e07011c52e38a9
Reece Kenney <reecekenney50@gmail.com>
2020-10-15 13:52:46 +0100

    passwordHash

--- routes/registerRoutes.js
+++ routes/registerRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 const bodyParser = require("body-parser")
+const bcrypt = require("bcrypt");
 const User = require('../schemas/UserSchema');
 
 app.set("view engine", "pug");
@@ -41,6 +42,8 @@ router.post("/", async (req, res, next) => {
             // No user found
 
             var data = req.body;
+
+            data.password = await bcrypt.hash(password, 10);
 
             User.create(data)
             .then((user) => {
