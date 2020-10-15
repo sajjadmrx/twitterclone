4667af3eaddba40a79fbec25a1d488d8466e3e0e
Reece Kenney <reecekenney50@gmail.com>
2020-10-15 11:16:18 +0100

    userSchema

--- database.js
+++ database.js
@@ -1,3 +1,9 @@
+//The require(‘mongoose’) call above returns a Singleton object. 
+//It means that the first time you call require(‘mongoose’), it 
+//is creating an instance of the Mongoose class and returning it. 
+//On subsequent calls, it will return the same instance that was 
+//created and returned to you the first time because of how module 
+//import/export works in ES6.
 const mongoose = require("mongoose");
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useUnifiedTopology', true);
