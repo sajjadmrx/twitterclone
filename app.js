7b80826b35ed6fedfc90d6a964f8b0ae6bf7be7a
Reece Kenney <reecekenney50@gmail.com>
2020-10-15 14:56:52 +0100

    sessions

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const middleware = require('./middleware')
 const path = require('path')
 const bodyParser = require("body-parser")
 const mongoose = require("./database");
+const session = require("express-session");
 
 const server = app.listen(port, () => console.log("Server listening on port " + port));
 
@@ -13,6 +14,12 @@ app.set("views", "views");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use(session({
+    secret: "bbq chips",
+    resave: true,
+    saveUninitialized: false
+}))
 
 // Routes
 const loginRoute = require('./routes/loginRoutes');
