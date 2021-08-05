0ce9134b575a7124cb284fd3b00106b5a5b7b3e8
sajjadmrx <sajjadzibaee102@gmail.com>
2021-08-04 19:42:43 -0700

    Inserting the post into the database

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -3,16 +3,31 @@ const express = require('express');
 const router = express.Router();
 
 
-const User = require('../../schemas/UserSchema');
+const userModel = require('../../schemas/UserSchema');
+const postsModel = require('../../schemas/PostSchema');
 
 
 
 
-router.post("/", (req, res, next) => {
+router.post("/", async (req, res, next) => {
     if (!req.body.content)
         return res.sendStatus(400)
 
-    res.status(200).send('it worked!')
+
+    const postData = {
+        content: req.body.content,
+        postedBy: req.session.user
+    }
+    try {
+        let newPost = await postsModel.create(postData)
+        newPost = await userModel.populate(newPost, { path: 'postedBy', select: '-password -email' })
+        res.status(201).send(newPost)
+    } catch (error) {
+        console.log(error)
+        res.status(400)
+    }
+
+
 })
 
 
